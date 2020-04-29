94e364271b6fb41fa3620e5b6e97acf0d1cee324
Austin LaClair <austinlaclair@austins-mbp.attlocal.net>
2020-04-28 21:26:20 -0400

    functional game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,22 +17,75 @@ $('document').ready(function() {
             music: "Musician",
         },
         correct: "Musician"
+    },{
+        question: "Edgar Allan Poe was born on what day?",
+        answers: {
+            jan5: "January 5th",
+            feb5: "February 5th",
+            jan19: "January 19th",
+            hallo: "October 31st",
+        },
+        correct: "January 19th"
     }
     ]
     let questionCounter = 0
+    let correct = 0
+    let incorrect = 0
+    let timeout = 0
+    let sec
     $('.start').click(function startGame() {
         $('.start').remove()
         nextQuestion()
     })
-    function nextQuestion() {    
-        $('.main-game').append(questions[questionCounter].question + '<br<')
+    function nextQuestion() {
+        if (questionCounter == questions.length) {
+        $('.main-game').empty().append('Game Over').append('<br>Correct Guesses: ' + correct).append('<br>Incorrect Guesses: ' + incorrect).append('<br>Timeouts: ' + timeout)
+        $('.main-game').append('<br><button class="restart">Restart</button>')
+        $('.restart').click(function restartGame() {
+            questionCounter = 0
+            correct = 0
+            incorrect = 0
+            timeout = 0
+            nextQuestion()
+        })
+        }
+        else {
+        let timer = setInterval(() => {
+        $('.timer').show()
+        $('.timer').text(sec--)
+        if(sec < 0) {
+            $('.main-game').empty().append('Foolish Mortal').append('<br> The correct answer was ' + questions[questionCounter].correct)
+            questionCounter++
+            timeout++
+            setTimeout(nextQuestion, 4000)
+            clearInterval(timer)
+            $('.timer').hide()
+        }
+        }, 1000);  
+        sec = 15
+        $('.main-game').empty()
+          
+        $('.main-game').append('<br>' + questions[questionCounter].question + '<br<')
         for (key in questions[questionCounter].answers) {
             $('.main-game').append('<br><button class="btn" value="' + questions[questionCounter].answers[key] + '">' + questions[questionCounter].answers[key] + '</button>')
         }
     $('.btn').click(function clicked() {
-        console.log(this)
+        if ($(this).val() == questions[questionCounter].correct) {
+        $('.main-game').empty().append('Nice Job Mortal')
+        questionCounter++
+        correct++
+        setTimeout(nextQuestion, 4000)
+        clearInterval(timer)
+        $('.timer').hide()
+        }
+        else {
+        $('.main-game').empty().append('Foolish Mortal').append('<br> The correct answer was ' + questions[questionCounter].correct)
+        questionCounter++
+        incorrect++
+        setTimeout(nextQuestion, 4000)
+        clearInterval(timer)
+        $('.timer').hide()
+        }
     })
-    }
-
-
+}}
 })
